e762d208dc7eb95f13d3267d53caf4430453523f
mitalasam <87754537+mitalasam@users.noreply.github.com>
2021-10-04 15:49:37 +0300

    Add files via upload

---
+++ sam.js
@@ -0,0 +1,68 @@
+// Declaring variables
+
+const btn = document.querySelector('.btn3');
+const java = document.querySelector('.java');
+const btn4 = document.querySelector('.btn4');
+const menu = document.querySelector('.menu')
+
+// calling variables
+
+menu.addEventListener('click', ()=>{
+links.classList.toggle('is-active');
+bar.classList.toggle('active');
+})
+btn.addEventListener('click', ()=>{
+set.textContent = 'welcome to my saloon';
+})
+console.log('change');
+
+
+
+
+
+console.log('java');
+btn4.addEventListener('click', ()=>{
+header2.textContent = 'welcome to my saloon';
+})
+// styling variables
+
+ header1.style.background = 'blue';
+btn1.style.background = 'brown';
+port.style.background = '#ccc';
+header1.style.color= 'white';
+btn2.addEventListener('click',()=>{
+ header2.style.color = 'blue';
+ })
+ font.style.color= 'pink';
+ header2.style.background= 'yellow';
+ header1.style.fontSize= '20px';
+ header1.style.backgroundImage="url('image/f7.jpg')"
+ header1.style.height='100px'
+// using functions
+function hello(){
+console.log('click');
+}
+hello()
+function boy(){
+console.log('girl are natured');
+}
+boy()
+let x=20
+console.log('20');
+
+const btn = document.querySelector('.nav');
+const btn = document.querySelector('#menu');
+
+sam.addEventListener('click', ()=>{
+ nav.show.toggle ='menu';
+ });
+
+
+         // Function to toggle the bar
+         function nav() {
+          var link = document.getElementById("link");
+          if (x.style.display === "block") {
+              x.style.display = "none";
+          } else {
+              x.style.display = "block";
+          }}
